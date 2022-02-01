02ec02ab030b379241b1609c03160603db07e2a6
Funisaki <funisakipurin@gmail.com>
2022-02-01 17:29:36 +0900

    ShaderNodeGroup対応

--- src/fsb_import_operator.py
+++ src/fsb_import_operator.py
@@ -2,6 +2,7 @@ import bpy
 import requests
 import xml.dom.minidom
 from . import rna_xml_alt
+import json
 
 class FSBImportOperator(bpy.types.Operator):
     bl_idname = "funi_shader_box.import"
@@ -9,44 +10,72 @@ class FSBImportOperator(bpy.types.Operator):
     bl_label = "ShaderBox"
     bl_region_type = "UI"
     bl_category = "ShaderBox" 
-    
-    def createNodesAndLinks(self,node_tree,root_xml):
+    def createShaderNodeGroup(self,name, xml_string):
+        with xml.dom.minidom.parseString(xml_string) as node_group_xml:
+            new_group = bpy.data.node_groups.new(name, "ShaderNodeTree")
+            self.node_group_map[name] = new_group.name
+            root_xml = node_group_xml.getElementsByTagName("ShaderNodeTree")[0]
+            self.createNodesAndLinks(new_group, root_xml,type="NODE")
+            self.createNodesAndLinks(new_group, root_xml,type="LINK")
+            rna_xml_alt.xml2rna(root_xml, root_rna=new_group)
+            self.createNodesAndLinks(new_group, root_xml,type="LINK")
+
+    def createNodesAndLinks(self,node_tree,root_xml, type="NODE"):
         nodes_xml = root_xml.getElementsByTagName("nodes")[0]
         links_xml = root_xml.getElementsByTagName("links")[0]
-        def create(xml, type="NODE"):
-            if xml.nodeType != xml.ELEMENT_NODE:
+        def create(node_xml, type="NODE"):
+            if node_xml.nodeType != node_xml.ELEMENT_NODE:
                 return
             if(type== "NODE"):
-                bpy.ops.node.add_node(type= xml.nodeName, use_transform=True)
-                node_tree.nodes[-1].name = xml.attributes["name"].value
+                new_node = node_tree.nodes.new(node_xml.nodeName)
+                new_node.name = node_xml.attributes["name"].value
             elif(type=="LINK"):
-                fromNodeId = xml.attributes["from_node"].value.split("::")[1]
-                fromSocketId = xml.getElementsByTagName("from_socket")[0].childNodes[1].attributes["identifier"].value
-                toNodeId = xml.attributes["to_node"].value.split("::")[1]
-                toSocketId = xml.getElementsByTagName("to_socket")[0].childNodes[1].attributes["identifier"].value
+                fromNodeId = node_xml.attributes["from_node"].value.split("::")[1]
+                fromSocketId = node_xml.getElementsByTagName("from_socket")[0].childNodes[1].attributes["identifier"].value
+                toNodeId = node_xml.attributes["to_node"].value.split("::")[1]
+                toSocketId = node_xml.getElementsByTagName("to_socket")[0].childNodes[1].attributes["identifier"].value
+                
+                if(len(node_tree.nodes.get(fromNodeId).outputs) == 0):
+                    return
                 fromSocket = node_tree.nodes.get(fromNodeId).outputs[0]
                 for inp in node_tree.nodes.get(fromNodeId).outputs:
                     if(fromSocketId == inp.identifier):
                         fromSocket = inp
+                if(len(node_tree.nodes.get(toNodeId).inputs) == 0):
+                    return
                 toSocket = node_tree.nodes.get(toNodeId).inputs[0]
                 for inp in node_tree.nodes.get(toNodeId).inputs:
                     if(toSocketId == inp.identifier):
                         toSocket = inp
                 node_tree.links.new(fromSocket,toSocket)
-        for xml in nodes_xml.childNodes:
-            create(xml, type="NODE")
-        for xml in links_xml.childNodes:
-            create(xml, type="LINK")
+        if(type == "NODE"):
+            for node_xml in nodes_xml.childNodes:
+                create(node_xml, type="NODE")
+        if(type == "LINK"):
+            for node_xml in links_xml.childNodes:
+                create(node_xml, type="LINK")
+        
         
     def execute(self, context):
+        self.node_group_map = {}
         scene = context.scene 
-        url = 'https://l.funisaki.com/shaderbox/%s/nodetree.xml' % scene.funiShadeBoxItemId
+        url = 'https://l.funisaki.com/shaderbox/%s/shaderdef.json' % scene.funiShadeBoxItemId
         x = requests.get(url)
-        xml_nodes = xml.dom.minidom.parseString(x.content)
-        root_xml = xml_nodes.getElementsByTagName("ShaderNodeTree")[0]
-        material = bpy.context.active_object.active_material
-        for n in material.node_tree.nodes:
-            material.node_tree.nodes.remove(n)
-        self.createNodesAndLinks(material.node_tree, root_xml)
-        rna_xml_alt.xml2rna(root_xml, root_rna=material.node_tree)
+        shader_def = json.loads(x.content)
+        node_group_list = shader_def["node_group_list"]
+        node_group_list.reverse()
+        root_node_tree_pack = node_group_list.pop()
+        for node_tree_pack in node_group_list:
+            xml_string = node_tree_pack["xml"]
+            self.createShaderNodeGroup(node_tree_pack["name"],xml_string)
+        with xml.dom.minidom.parseString(root_node_tree_pack["xml"]) as xml_nodes:
+            root_xml = xml_nodes.getElementsByTagName("ShaderNodeTree")[0]
+            material = bpy.context.active_object.active_material
+            for n in material.node_tree.nodes:
+                material.node_tree.nodes.remove(n)
+            self.createNodesAndLinks(material.node_tree, root_xml, type="NODE")
+            self.createNodesAndLinks(material.node_tree, root_xml, type="LINK")
+            rna_xml_alt.xml2rna(root_xml, root_rna=material.node_tree)
+            self.createNodesAndLinks(material.node_tree, root_xml, type="LINK")
+        
         return{'FINISHED'}
--- src/fsb_upload_operator.py
+++ src/fsb_upload_operator.py
@@ -4,6 +4,7 @@ import requests
 import base64
 import os
 from . import rna_xml_alt
+import json
 
 PREVIEW_SCENE_PATH = os.path.join(os.path.dirname(__file__), "preview.blend","Scene")
 UPLOAD_URL = 'https://tools.funisaki.com/api/shaderbox/upload'
@@ -15,26 +16,45 @@ class FSBUploadOperator(bpy.types.Operator):
     bl_region_type = "UI"
     bl_category = "ShaderBox"
 
+    def read_node_tree(self, temp_path, node_tree, tree_list):
+        node_tree_dups = 0
+        for t in tree_list:
+            if(t["name"] == node_tree.name):
+                node_tree_dups = 1
+                break
+        if(node_tree_dups == 1):
+            return
+
+        nodetree_file_path = temp_path + str(len(tree_list)) + "node_tree.xml"
+        
+        f = open(nodetree_file_path,"w+")
+        rna_xml_alt.rna2xml(fw=f.write, root_node="Root", root_rna=node_tree, method='ATTR')
+        f.close()
+        with open(nodetree_file_path, "rb") as xml_file:
+            xml = xml_file.read().decode('utf-8')
+        tree_list.append({'name': node_tree.name, 'xml': xml})
+        for n in node_tree.nodes:
+            if(n.type == "GROUP"):
+                self.read_node_tree(temp_path, n.node_tree, tree_list)
+
     def execute(self, context):
         temp_path = bpy.app.tempdir
         preview_file_path = temp_path + "funishaderboxpreviewresult.png"
-        nodetree_file_path = temp_path + "node_tree.xml"
         material = bpy.context.active_object.active_material
         bpy.ops.wm.append(filepath=PREVIEW_SCENE_PATH + "ShaderBoxPreviewScene", directory=PREVIEW_SCENE_PATH, filename="ShaderBoxPreviewScene")
         bpy.data.scenes["ShaderBoxPreviewScene"].render.filepath = preview_file_path
         bpy.data.scenes["ShaderBoxPreviewScene"].objects[2].data.materials[0] = material
-        f = open(nodetree_file_path,"w+")
-        rna_xml_alt.rna2xml(fw=f.write, root_node="Root", root_rna=material.node_tree, method='ATTR')
-        f.close()
-        
+        node_group_list = list()
+        self.read_node_tree(temp_path, material.node_tree, node_group_list)
+
         bpy.ops.render.render(animation=False, write_still=True, use_viewport=False, layer='', scene="ShaderBoxPreviewScene")
         bpy.ops.scene.delete({"scene": bpy.data.scenes["ShaderBoxPreviewScene"]})
-        with open(nodetree_file_path, "rb") as xml_file:
-            xml_base64 = base64.b64encode(xml_file.read())
+        
         with open(preview_file_path, "rb") as image_file:
             image_base64 = base64.b64encode(image_file.read())
-        myobj = {"xml": xml_base64,"image": image_base64}
-        with requests.post(UPLOAD_URL, data = myobj) as resp:
+        url = 'https://tools.funisaki.com/api/shaderbox/upload'
+        myobj = {"shader_def": json.dumps({"node_group_list": node_group_list}, separators=(',', ':')) ,"image": image_base64}
+        with requests.post(url, data = myobj) as resp:
             res = resp
         data = res.json()
         webbrowser.open(data["url"])
