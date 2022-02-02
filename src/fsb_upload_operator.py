7636e73df3a0e6e79a36de345dfedb702a9116e0
Funisaki <funisakipurin@gmail.com>
2022-02-02 23:04:03 +0900

    プレビュー時に作られたリソースを削除する

--- src/fsb_upload_operator.py
+++ src/fsb_upload_operator.py
@@ -16,6 +16,21 @@ class FSBUploadOperator(bpy.types.Operator):
     bl_region_type = "UI"
     bl_category = "ShaderBox"
 
+    def delete_preview_scene(self, scene):
+        bpy.data.lights.remove(bpy.data.lights[-1])
+        bpy.data.cameras.remove(bpy.data.cameras[-1])
+        bpy.data.objects.remove(scene.objects[0])
+        bpy.data.objects.remove(scene.objects[0])
+        bpy.data.collections.remove(bpy.data.collections[-1])
+        bpy.data.worlds.remove(bpy.data.worlds[-1])
+        bpy.data.linestyles.remove(bpy.data.linestyles[-1])
+        bpy.data.meshes.remove(bpy.data.meshes[-1])
+        bpy.data.meshes.remove(bpy.data.meshes[-1])
+        bpy.data.materials.remove(bpy.data.materials[-1])
+        bpy.data.materials.remove(bpy.data.materials[-1])
+        bpy.data.materials.remove(bpy.data.materials[-1])
+        bpy.ops.scene.delete({"scene": scene})
+    
     def read_node_tree(self, temp_path, node_tree, tree_list):
         node_tree_dups = 0
         for t in tree_list:
@@ -41,15 +56,15 @@ class FSBUploadOperator(bpy.types.Operator):
         temp_path = bpy.app.tempdir
         preview_file_path = temp_path + "funishaderboxpreviewresult.png"
         material = bpy.context.active_object.active_material
+
         bpy.ops.wm.append(filepath=PREVIEW_SCENE_PATH + "ShaderBoxPreviewScene", directory=PREVIEW_SCENE_PATH, filename="ShaderBoxPreviewScene")
-        bpy.data.scenes["ShaderBoxPreviewScene"].render.filepath = preview_file_path
-        bpy.data.scenes["ShaderBoxPreviewScene"].objects[2].data.materials[0] = material
+        preview_scene = bpy.data.scenes[-1]
+        preview_scene.render.filepath = preview_file_path
+        preview_scene.objects[2].data.materials[0] = material
         node_group_list = list()
         self.read_node_tree(temp_path, material.node_tree, node_group_list)
-
-        bpy.ops.render.render(animation=False, write_still=True, use_viewport=False, layer='', scene="ShaderBoxPreviewScene")
-        bpy.ops.scene.delete({"scene": bpy.data.scenes["ShaderBoxPreviewScene"]})
-        
+        bpy.ops.render.render(animation=False, write_still=True, use_viewport=False, layer='', scene=preview_scene.name)
+        self.delete_preview_scene(preview_scene)
         with open(preview_file_path, "rb") as image_file:
             image_base64 = base64.b64encode(image_file.read())
         url = 'https://tools.funisaki.com/api/shaderbox/upload'
